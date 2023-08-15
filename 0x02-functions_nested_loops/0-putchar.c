0a2abca04cd9a4b6052158fea8a484aec9eec8ee
asenjialvin <asenjialvin@gmail.com>
2023-08-15 10:54:40 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    char text[] = "_putchar";
+    char c[] = "_putchar";
     int i = 0;
 
-    while (text[i] != '\0')
+    while (c[i] != '\0')
     {
-        putchar(text[i]);  // Use putchar function to print each character
+        putchar(c[i]);  // putchar function to print each character
         i++;
     }
-    putchar('\n');  // Use putchar to print a newline character
+    putchar('\n');  // putchar to print a newline character
 
     return (0);
 }
