87f3b74d5b0452c420a21a6d1ae655c7548d09e9
asenjialvin <asenjialvin@gmail.com>
2023-09-22 13:01:09 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,53 +11,61 @@
  * Return: the address of the new element, or NULL if it fails
  */
 
+typedef struct list_s
+{
+       	char *str;
+	struct list_s *next;
+}
+list_t;
+
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    
-    if (str == NULL)
-    {
-        return NULL;
-    }
-    
-    new_node = (list_t *)malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
-    
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
-    
-    new_node->next = *head;
-    *head = new_node;
-    
-    return new_node;
+	if (str == NULL)
+	{
+		return NULL;
+	}
+	
+list_t *new_node = (list_t *)malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+	return NULL;
+}
+
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+	free(new_node);
+	return NULL;
+}
+
+new_node->next = *head;
+*head = new_node;
+
+return new_node;
 }
 
 int main()
 {
-    list_t *head = NULL;
-    list_t *current;
+	list_t *head = NULL;
+	
+	add_node(&head, "Node 1");
+	add_node(&head, "Node 2");
+	add_node(&head, "Node 3");
 
-    current = head;
-    while (current != NULL)
-    {
-        printf("%s\n", current->str);
-        current = current->next;
-    }
+	list_t *current = head;
+	while (current != NULL)
+	{
+		printf("%s\n", current->str);
+		current = current->next;
+	}
 
-    current = head;
-    while (current != NULL)
-    {
-        list_t *temp = current;
-        current = current->next;
-        free(temp->str);
-        free(temp);
-    }
-    return 0;
+	current = head;
+	while (current != NULL)
+	{
+		list_t *temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+	return 0;
 }
