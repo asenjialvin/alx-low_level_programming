f8e42dbb789dcd47c593edbf64b3fe7e22d46c66
asenjialvin <asenjialvin@gmail.com>
2023-09-23 10:25:54 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,71 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
- *
- * Return: the address of the new element, or NULL if it fails
+ * add_node - add a new node at the beginning of `list_t` list
+ * @head: double pointer to head
+ * @str: string to be saved in new node, must be duplicated
+ * Return: Address of new element or NULL if failed
  */
-
-typedef struct list_s
-{
-       	char *str;
-	struct list_s *next;
-}
-list_t;
-
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
+	list_t *new_node;
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
-		return NULL;
+		free(new_node);
+		return (NULL);
 	}
-	
-list_t *new_node = (list_t *)malloc(sizeof(list_t));
-if (new_node == NULL)
-{
-	return NULL;
-}
-
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-	free(new_node);
-	return NULL;
-}
-
-new_node->next = *head;
-*head = new_node;
-
-return new_node;
-}
-
-int main()
-{
-	list_t *head = NULL;
-	
-	add_node(&head, "Node 1");
-	add_node(&head, "Node 2");
-	add_node(&head, "Node 3");
-
-	list_t *current = head;
-	while (current != NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		printf("%s\n", current->str);
-		current = current->next;
+		free(new_node);
+		return (NULL);
 	}
+	new_node->len = c;
+	new_node->next = *head;
 
-	current = head;
-	while (current != NULL)
-	{
-		list_t *temp = current;
-		current = current->next;
-		free(temp->str);
-		free(temp);
-	}
-	return 0;
+	*head = new_node;
+	return (*head);
 }
