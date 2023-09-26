2b025719b6f37b8dcd16376afffc74a01f68035c
asenjialvin <asenjialvin@gmail.com>
2023-09-25 22:51:59 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,33 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - free a `listint_t` list and set the head to null
- * @h: double pointer to head of linked list
- * Description: This function should work for circular lists
- * Only loop through the list once
- * Return: size of the list that was free'd
+ * free_listint_safe - Frees a linked list of type listint_t.
+ * @h: A pointer to a pointer to the head of the linked list.
+ *
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	listint_t *current, *temp;
+	size_t count = 0;
 
-	count = 0;
+	if (h == NULL || *h == NULL)
+		return (0);
+
 	current = *h;
 	while (current != NULL)
 	{
+		temp = current->next;
+		free(current);
 		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
 
-		if (hold < current)
+		if (temp >= current)
 			break;
-	}
-	*h = NULL;
 
+		current = temp;
+	}
+
+	*h = NULL;
 	return (count);
 }
